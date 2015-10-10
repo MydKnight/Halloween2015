5cf2bc55f1149633050089d302ce8c4155bc35e9
madsens <shiloh.madsen@gmail.com>
2015-10-09 22:49:59 -0700

    Final of Fireflies Gag...I think.

--- Lights.py
+++ Lights.py
@@ -31,7 +31,7 @@ def showColor(color):
 def activatePins(pinArray):
     for pin in pinArray:
         GPIO.output(pin, True)
-    time.sleep(8)
+    time.sleep(9)
     #Then set all pins back to false.
     GPIO.output(11, False)
     GPIO.output(13, False)

059152efb027bb2b63b0e10df0e183a83c7576f6
madsens <shiloh.madsen@gmail.com>
2015-10-10 23:54:06 -0700

    Second test of DMX Libs

--- Trixie.py
+++ Trixie.py
@@ -1,9 +1,13 @@
 __author__ = 'madsens'
-import DMX
+from ola.ClientWrapper import ClientWrapper
+import array
 
-dmx=dmx512.dmx
-dmx.set_channel(1,255)
-dmx.set_channel(419,39)
-dmx.send()
+def DmxSent(state):
+  wrapper.Stop()
 
-dmx.channel[21]
+universe = 1
+data = array.array('B', [10, 50, 255])
+wrapper = ClientWrapper()
+client = wrapper.Client()
+client.SendDmx(universe, data, DmxSent)
+wrapper.Run()

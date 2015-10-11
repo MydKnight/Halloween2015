f056082763ac86a9e650c3d28e2a050805d149b7
madsens <shiloh.madsen@gmail.com>
2015-10-11 00:01:26 -0700

    Continued uncommenting

--- Trixie.py
+++ Trixie.py
@@ -6,8 +6,8 @@ def DmxSent(state):
   wrapper.Stop()
 
 universe = 1
-#data = array.array('B', [10, 50, 255])
-#wrapper = ClientWrapper()
+data = array.array('B', [10, 50, 255])
+wrapper = ClientWrapper()
 #client = wrapper.Client()
 #client.SendDmx(universe, data, DmxSent)
 #wrapper.Run()

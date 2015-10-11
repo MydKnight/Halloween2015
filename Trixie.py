53b4ebb9099978ce616ace88039edc069961ad9d
madsens <shiloh.madsen@gmail.com>
2015-10-10 23:58:51 -0700

    Commented out code

--- Trixie.py
+++ Trixie.py
@@ -2,12 +2,12 @@ __author__ = 'madsens'
 from ola.ClientWrapper import ClientWrapper
 import array
 
-def DmxSent(state):
-  wrapper.Stop()
+#def DmxSent(state):
+#  wrapper.Stop()
 
-universe = 1
-data = array.array('B', [10, 50, 255])
-wrapper = ClientWrapper()
-client = wrapper.Client()
-client.SendDmx(universe, data, DmxSent)
-wrapper.Run()
+#universe = 1
+#data = array.array('B', [10, 50, 255])
+#wrapper = ClientWrapper()
+#client = wrapper.Client()
+#client.SendDmx(universe, data, DmxSent)
+#wrapper.Run()

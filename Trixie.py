0ecbb33c30581dbb08dfb45df838c41d03117542
madsens <shiloh.madsen@gmail.com>
2015-10-12 18:36:11 -0700

    Changing the varialbe replacement to string.

--- Trixie.py
+++ Trixie.py
@@ -24,7 +24,7 @@ while True:    # Runs until break is encountered. We want to set it to break on 
             f.extend(filenames)
             break
         file = random.choice(f)
-        os.system('mpg321 %d &' % file)
+        os.system('mpg321 %s &' % file)
 
         #Trigger GPIO Pins. Trixie just uses pin 13
         Lights.activatePins([13])

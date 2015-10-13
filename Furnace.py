500d2adc0124f309ac3f592610e7eb89556b2ebc
madsens <shiloh.madsen@gmail.com>
2015-10-12 22:26:11 -0700

    Turning videos back on.

--- Furnace.py
+++ Furnace.py
@@ -4,21 +4,20 @@ import Logging
 import Movies
 
 Lights.setup()
-#Movies.StartLoop()
+Movies.StartLoop()
 
 while True:    # Runs until break is encountered. We want to set it to break on a particular ID.
     n = raw_input("Scanned ID: ")
     if n == "0001603911":
-        #Movies.StopLoop()
+        Movies.StopLoop()
         Lights.cleanup()
         break  # stops the loop
     else :
+        #Log Activation of PI
+        Logging.LogAccess(n)
+
         #Trigger GPIO Pins. Fogger on 13
         Lights.activatePins([13])
 
         #Play Furnace Video - Test
-        #Movies.PlayMovie()
-
-        #Log Activation of PI
-        print "RFID: " + n
-        Logging.LogAccess(n)
+        Movies.PlayMovie()

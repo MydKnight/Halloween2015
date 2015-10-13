c74ed850d53191991aa7ab91c41966eee34dcfcf
madsens <shiloh.madsen@gmail.com>
2015-10-12 18:34:48 -0700

    errors with random.

--- Trixie.py
+++ Trixie.py
@@ -23,7 +23,8 @@ while True:    # Runs until break is encountered. We want to set it to break on 
         for (dirpath, dirnames, filenames) in walk("Assets/Trixie"):
             f.extend(filenames)
             break
-        os.system('mpg321 %d &' %random.choice(f))
+        file = random.choice(f)
+        os.system('mpg321 %d &' % file)
 
         #Trigger GPIO Pins. Trixie just uses pin 13
         Lights.activatePins([13])

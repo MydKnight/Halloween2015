c715208c99462409d7a27a755e78e7415dcd12fa
madsens <shiloh.madsen@gmail.com>
2015-10-23 18:36:41 -0700

    Added correct audio triggers

--- Fireflies.py
+++ Fireflies.py
@@ -4,13 +4,12 @@ import Lights
 import time
 import Logging
 from subprocess import Popen, PIPE
-import AudioRandomizer
 # This code runs the fireflies gag
 
 lastScan = 0
 Lights.setup()
 previousFile = ""
-Popen(['mpg321', 'Assets/CreepyLaugh.mp3', '--loop', '0'], stdout=PIPE, close_fds=True)
+Popen(['mpg321', 'Assets/Crickets.mp3', '--loop', '0'], stdout=PIPE, close_fds=True)
 
 while True:    # Runs until break is encountered. We want to set it to break on a particular ID.
     n = raw_input("Scanned ID: ")
@@ -23,30 +22,24 @@ while True:    # Runs until break is encountered. We want to set it to break on 
         if currentScan - lastScan < 15:
             print "Elapsed Time: ", currentScan - lastScan
             #Log Activation of PI - No Audio
-            Logging.LogAccess(n)
+            #Logging.LogAccess(n)
         else:
-            #kill crickets loop, play random file from Fireflies folder
-            Popen(['sudo', 'pkill', 'mpg321'], stdout=PIPE, close_fds=True)
-            time.sleep(1)
-            previousFile = AudioRandomizer.PlayRandomAudio("Assets/Trixie/", previousFile)
-            print previousFile
+            Popen(['mpg321', 'Assets/CreepyLaugh.mp3'], stdout=PIPE, close_fds=True)
             #Log Activation of PI
-            Logging.LogAudioAccess(n, previousFile)
+            #Logging.LogAccess(n)
 
         #Trigger GPIO Pins. Do mod3 on the card. If 0, X and Y, if 1 trigger X, if 2 trigger Y.
         num=int(n)
         if num%3 == 0:
-            Lights.activatePins([15])
+            Lights.activatePins([33])
             time.sleep(9)
         elif num%3 == 1:
-            Lights.activatePins([11])
+            Lights.activatePins([35])
             time.sleep(9)
         elif num%3 == 2:
-            Lights.activatePins([13])
+            Lights.activatePins([37])
             time.sleep(9)
         print "Modulo: ",num%3
-
-        Popen(['mpg321', 'Assets/CreepyLaugh.mp3', '--loop', '0'], stdout=PIPE, close_fds=True)
 
         lastScan = currentScan
 

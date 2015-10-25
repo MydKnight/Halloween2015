227f5dc1290e45d915aa8560b1be4f96e033169c
madsens <shiloh.madsen@gmail.com>
2015-10-24 19:03:16 -0700

    Fixed with termios. This should work now.

--- BrawlFight.py
+++ BrawlFight.py
@@ -5,12 +5,12 @@ import time
 
 import select,sys,time,termios
 
-#Movies.StartLoop('/home/pi/Halloween2015/Assets/BrawlFight')
+Movies.StartLoop('/home/pi/Halloween2015/Assets/BrawlFight')
 
 while True:    # Runs until break is encountered. We want to set it to break on a particular ID.
     n = raw_input("Scanned ID: ")
     if n == "STOP":
-        #Movies.StopLoop()
+        Movies.StopLoop()
         break  # stops the loop
     else :
         print "You typed: ", n
@@ -18,10 +18,8 @@ while True:    # Runs until break is encountered. We want to set it to break on 
         #Logging.LogAccess(n)
 
         #Play Furnace Video - Test
-        #Movies.PlayMovie()
+        Movies.PlayMovie()
 
         #Wait until Video finishes
-        #time.sleep(20)
-
-        time.sleep(10)
+        time.sleep(20)
         termios.tcflush(sys.stdin,termios.TCIFLUSH)

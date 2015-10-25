3faa2804331d3cce13b217cbb76c4ac7be1ee88c
madsens <shiloh.madsen@gmail.com>
2015-10-24 22:32:54 -0700

    Making it quiet

--- Bridge.py
+++ Bridge.py
@@ -5,6 +5,6 @@ import random
 
 while True:
     rnd = random.randint(15,600)
-    Popen(['mpg321', 'Assets/BridgeHorse.mp3', '--loop', '0'], stdout=PIPE, close_fds=True)
+    Popen(['mpg321', 'Assets/BridgeHorse.mp3', '--gain', '10'], stdout=PIPE, close_fds=True)
     print "Sleeping for %i seconds." %rnd
     time.sleep(rnd)

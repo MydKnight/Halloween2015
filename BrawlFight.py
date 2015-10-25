8f14cc239ccfe242b409f4ef9b939908cea4c990
madsens <shiloh.madsen@gmail.com>
2015-10-24 18:30:03 -0700

    Added KL code to test

--- BrawlFight.py
+++ BrawlFight.py
@@ -2,8 +2,12 @@ __author__ = 'madsens'
 #import Logging
 import Movies
 import time
+import KeyboardLocker
 
 #Movies.StartLoop('/home/pi/Halloween2015/Assets/BrawlFight')
+kl = KeyboardLocker(serio=0)
+device = kl.description()
+print "We got a lock on", device
 
 while True:    # Runs until break is encountered. We want to set it to break on a particular ID.
     n = raw_input("Scanned ID: ")
@@ -12,6 +16,7 @@ while True:    # Runs until break is encountered. We want to set it to break on 
         break  # stops the loop
     else :
         print "You typed: ", n
+        kl.turn_off()
         #Log Activation of PI - Disabled until we're configured on the castle network
         #Logging.LogAccess(n)
 
@@ -20,3 +25,4 @@ while True:    # Runs until break is encountered. We want to set it to break on 
 
         #Wait until Video finishes
         time.sleep(20)
+        kl.turn_on()

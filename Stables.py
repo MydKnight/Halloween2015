bcef457b5243745054ca0c7b4bf4c0dd2fb44c38
madsens <shiloh.madsen@gmail.com>
2015-10-25 10:41:57 -0700

    First pass Stables

---
+++ Stables.py
@@ -0,0 +1,71 @@
+__author__ = 'madsens'
+
+import time
+import os
+from subprocess import Popen, PIPE
+import Lights
+
+# Setup Pins - May need to use Setup2 depending on if not reversed
+Lights.setup()
+
+# Turn the reader back on.
+os.system("/home/pi/Halloween2015/Scripts/enableRFID.sh")
+
+# Loop Barn Noises
+Popen(['mpg321', '/home/pi/Halloween2015/Assets/Stables/Barn.mp3', '--loop', '0'], stdout=PIPE, close_fds=True)
+
+while True:    # Runs until break is encountered. We want to set it to break on a particular ID.
+    n = raw_input("Scanned ID: ")
+    if n == "STOP":
+        break  # stops the loop
+    else :
+        print "You typed: ", n
+        #Turn off the reader until function finishes.
+        os.system("/home/pi/Halloween2015/Scripts/disableRFID.sh")
+
+        #Stop Barn Noises
+        Popen(['sudo' ,'pkill', 'mpg321'])
+        time.sleep(1)
+
+        #Play Horseman Audio
+        Popen(['mpg321', '/home/pi/Halloween2015/Assets/Stables/Horse.mp3'], stdout=PIPE, close_fds=True)
+
+        #Trigger Horse Eyes
+        Lights.on([35])
+
+        #Trigger Horse Nose
+        time.sleep(4)
+        Lights.on([37])
+        time.sleep(1)
+        Lights.off([37])
+        time.sleep(1)
+        Lights.on([37])
+        time.sleep(1)
+        Lights.off([37])
+        time.sleep(1)
+        Lights.on([37])
+        time.sleep(1)
+        Lights.off([37])
+        time.sleep(1)
+
+        #Trigger Barn Door Flapping
+        Lights.on([33])
+        time.sleep(1)
+        Lights.off([33])
+        time.sleep(1)
+        Lights.on([33])
+        time.sleep(1)
+        Lights.off([33])
+        time.sleep(1)
+        Lights.on([33])
+        time.sleep(1)
+        Lights.off([33])
+
+        #Wait?
+        time.sleep(3)
+
+        #Restart Barn Noises
+        Popen(['mpg321', '/home/pi/Halloween2015/Assets/Stables/Barn.mp3', '--loop', '0'], stdout=PIPE, close_fds=True)
+
+        #Turn the reader back on.
+        os.system("/home/pi/Halloween2015/Scripts/enableRFID.sh")

d63ca8cd46b40fd35c5f6e4a7c8723ebb3c15eff
madsens <shiloh.madsen@gmail.com>
2015-10-24 19:35:44 -0700

    New way to do it with bind/unbind.

--- BrawlFight.py
+++ BrawlFight.py
@@ -2,8 +2,7 @@ __author__ = 'madsens'
 #import Logging
 import Movies
 import time
-
-import select,sys,time,termios
+import os
 
 Movies.StartLoop('/home/pi/Halloween2015/Assets/BrawlFight')
 
@@ -17,10 +16,15 @@ while True:    # Runs until break is encountered. We want to set it to break on 
         #Log Activation of PI - Disabled until we're configured on the castle network
         #Logging.LogAccess(n)
 
+        #Turn off the reader until function finishes.
+        os.system("echo '1-1.4; |sudo tee /sys/bus/drivers/usb/unbind")
+
         #Play Furnace Video - Test
         Movies.PlayMovie()
 
-        #Wait until Video finishes
-        for x in range (0,20):
-            time.sleep(1)
-            termios.tcflush(sys.stdin,termios.TCIFLUSH)
+        time.sleep(20)
+
+        #Turn the reader back on.
+        os.system("echo '1-1.4; |sudo tee /sys/bus/drivers/usb/unbind")
+
+

3c9300923d9b1e65f518b8560624daae1dfc3087
madsens <shiloh.madsen@gmail.com>
2015-10-24 17:31:43 -0700

    Still fixing BarBrawl

--- BrawlFight.py
+++ BrawlFight.py
@@ -5,7 +5,7 @@ import Movies
 import time
 
 #Lights.setup()
-Movies.StartLoop('/home/pi/Halloween2015/Assets/BarBrawl')
+#Movies.StartLoop('/home/pi/Halloween2015/Assets/BarBrawl')
 
 while True:    # Runs until break is encountered. We want to set it to break on a particular ID.
     n = raw_input("Scanned ID: ")
@@ -14,11 +14,12 @@ while True:    # Runs until break is encountered. We want to set it to break on 
         #Lights.cleanup()
         break  # stops the loop
     else :
+        print "You typed: ", n
         #Log Activation of PI - Disabled until we're configured on the castle network
         #Logging.LogAccess(n)
 
         #Play Furnace Video - Test
-        Movies.PlayMovie()
+        #Movies.PlayMovie()
 
         #Wait until the Air Cannon should fire
         time.sleep(10)

40579c1edbe64340809619058c476093c81e2d4f
madsens <shiloh.madsen@gmail.com>
2015-10-28 15:44:30 -0700

    fixing head drop

--- DinRmClouds.py
+++ DinRmClouds.py
@@ -9,6 +9,8 @@ from DmxPy import DmxPy
 
 dmx = DmxPy('/dev/ttyUSB0')
 Lights.setup()
+Lights.off([37])
+
 previousFile = ""
 lastScan = 0
 TIMEOUT = 540
@@ -84,7 +86,7 @@ def input():
             time.sleep(4)
             #Trigger GPIO Pins. Head Chop on 15
             print "Drop Head"
-            Lights.off([37])
+            Lights.on([37])
             os.system('mpg321 /home/pi/Halloween2015/Assets/HorsemanSlashes.mp3 -q')
             time.sleep(2)
 
@@ -98,7 +100,7 @@ def input():
 
             #Reset Heads GPIO 11 then bring blacklight back up after 5
             print" Raising Head"
-            Lights.on([37])
+            Lights.off([37])
 
             #Restart Audio
             os.system('mpg321 /home/pi/Halloween2015/Assets/Thunder/RagingWinds.mp3 --loop 0 --gain 30 -q &')

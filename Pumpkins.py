26bd4640e4a8a6ffc8e6f025f6a77e98355ae52a
madsens <shiloh.madsen@gmail.com>
2015-10-28 17:39:59 -0700

    Fixed alternate pumpkin bad joke

--- Pumpkins.py
+++ Pumpkins.py
@@ -115,6 +115,8 @@ def input():
             playing = MP3("/home/pi/Halloween2015/Assets/PumpkinAudio/%i.mp3" %previousFile)
             if previousFile == 22:
                 previousFile = 1
+            elif previousFile == 10:
+                previousFile +2
             else:
                 previousFile += 1
             print "Sleeping for: %i seconds.\n" %playing.info.length
